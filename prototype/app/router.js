67f69c246df0ffec15004ff405f6cd484b231840
황진홍 <william1412.surya@gmail.com>
2023-05-18 22:03:08 +0700

    update booking page, history, minor update :(

--- prototype/app/router.js
+++ prototype/app/router.js
@@ -46,6 +46,24 @@ export const router = async (request, response) =>{
     response.setHeader('Content-Type', 'image');
     response.end(image);
   }
+  else if(request.url == "/spa-image"){
+    const image = await fs.readFile('./includes/images/spa.jpg');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'image');
+    response.end(image);
+  }
+  else if(request.url == "/massage-image"){
+    const image = await fs.readFile('./includes/images/massage.jpg');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'image');
+    response.end(image);
+  }
+  else if(request.url == "/refleksi-image"){
+    const image = await fs.readFile('./includes/images/refleksi.jpg');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'image');
+    response.end(image);
+  }
   else if(request.url == "/signed"){
     const content = await fs.readFile('./pages/index-signed.html', 'utf-8');
     response.statusCode = 200;
@@ -54,6 +72,42 @@ export const router = async (request, response) =>{
   }
   else if(request.url == "/log-in"){
     const content = await fs.readFile('./pages/login.html', 'utf-8');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(content);
+  }
+  else if(request.url == "/book-spa"){
+    const content = await fs.readFile('./pages/book-spa.html', 'utf-8');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(content);
+  }
+  else if(request.url == "/book-massage"){
+    const content = await fs.readFile('./pages/book-massage.html', 'utf-8');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(content);
+  }
+  else if(request.url == "/book-refleksi"){
+    const content = await fs.readFile('./pages/book-refleksi.html', 'utf-8');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(content);
+  }
+  else if(request.url == "/bookcss"){
+    const content = await fs.readFile('./includes/css/book.css', 'utf-8');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(content);
+  }
+  else if(request.url == "/book-history"){
+    const content = await fs.readFile('./pages/book-history.html', 'utf-8');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(content);
+  }
+  else if(request.url == "/historycss"){
+    const content = await fs.readFile('./includes/css/history.css', 'utf-8');
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html');
     response.end(content);
